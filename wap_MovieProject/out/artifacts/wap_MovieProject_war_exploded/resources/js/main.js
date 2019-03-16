80fd7e973218a94ef50689a9128dd6b2bfbe1be6
Tenhu <tenhu@tenhus-mbp.cs.mum.edu>
2019-03-16 10:37:12 -0500

    beta1

---
+++ wap_MovieProject/out/artifacts/wap_MovieProject_war_exploded/resources/js/main.js
@@ -0,0 +1,15 @@
+
+(function() {
+
+    alert('Hi1');
+
+    $("#header").hover(function () {
+        alert("hi2");
+
+    });
+    // $('.time').mouseout(
+    //     function () {
+    //         $(this).css({'background-color': 'none'});
+    //     }
+    // );
+})();
---
+++ wap_MovieProject/src/main/java/mum/model/TimeModel.java
@@ -0,0 +1,13 @@
+package mum.model;
+
+public class TimeModel {
+    String stime;
+
+    public TimeModel(String time) {
+        this.stime = time;
+    }
+
+    public String getStime() {
+        return stime;
+    }
+}
